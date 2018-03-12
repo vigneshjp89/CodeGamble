d74c57058c0e74202431132f274f915760818f78
vigneshjp89 <vickyjaya007@gmail.com>
2018-03-12 19:41:11 +0530

    first commit

---
+++ javascript/ScriptReg.js
@@ -0,0 +1,3 @@
+var pass=document.regisForm.pword.value;
+var rpass=document.regisForm.rpword.value;
+function check(var)
---
+++ php/zoho.php
@@ -0,0 +1,53 @@
+
+  <?php
+$userName=$_POST['uname'];
+$password=$_POST['pword'];
+$repassword=$_POST['rpword'];
+ /*$userName=stripslashes($userName);
+ $password=stripslashes($password);
+ $userName=mysql_real_escape_string($userName);
+ $password=mysql_real_escape_string($password);*/
+ //sql connect
+ if($password!=$repassword){
+   echo "password didnt match";
+   return;
+ }
+ $mysqli=new mysqli("localhost","root","","login");
+$result=$mysqli->query("select * from users where username='$userName'");
+$has=null;
+while($isthere=mysqli_fetch_assoc($result)){
+$has=$isthere['username'];
+}
+
+if(!($has==null)){
+  echo $has."-->Already exist";
+}
+else {
+  $mysqli->query("INSERT INTO `users` (`id`, `username`, `password`) VALUES (NULL, '$userName', '$password')");
+  header("Refresh:0; url=../login.html");
+}
+//$row=mysqli_fetch_array($result);
+
+while($row=mysqli_fetch_assoc($result)){
+  echo $row['username']."</br>";
+  echo $row['password']."</br>";
+}
+
+ //query
+/* $result=mysql_query("select * from users where username='$userName' and password='$password'")
+ or die("Failed to query database".mysql_error());
+ $row=mysql_fetch_array($result);
+ if($row['username']==$userName && $row['password']==$password){
+   echo "Hello".$row['username'];
+ }
+ else{
+   echo"failed to login";
+ }*/
+
+
+   ?>
+<script >
+function changeInner(){
+  document.getElementById("emailmod").innerHTML = "Email id Already Exist";
+}
+</script>
